8e9dc43db738dc695843a69e353bba8038bdb780
Md Risfat <Risfat.bd@gmail.com>
2023-09-11 17:28:58 +0600

    Init...

---
+++ android/app/src/main/kotlin/dev/risfat/google_maps_riverpod/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.risfat.google_maps_riverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+class Constants{
+  static String mapApiKey = "AIzaSyBzq63vbXtHhd458oNr9_NNdGV-lraUwUc";
+  // static String mapApiKey = "AIzaSyBzq63vbXtHhd458oNr9_NNdGV-lraUwUc";
+}
